3b490f1ee11ab2fb9304939774116527b01d9f72
Ethan <wowsilverhoof2@gmail.com>
2020-10-31 00:19:22 +1300

    Commit of MVS folder for second DLL

--- ArmaSpotifyJukebox/ArmaSpotifyJukebox/dllmain.cpp
+++ ArmaSpotifyJukebox/ArmaSpotifyJukebox/dllmain.cpp
@@ -16,9 +16,6 @@ extern "C"
 
 	// STRING callExtension STRING
 	__declspec(dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function);
-
-	// STRING callExtension ARRAY
-	__declspec(dllexport) int __stdcall RVExtensionArgs(char *output, int outputSize, const char *function, const char **argv, int argc);
 }
 
 // Called when the extension is first loaded
@@ -26,7 +23,7 @@ void RVExtensionVersion(char *output, int outputSize)
 {
 	// Reduce output by 1 to avoid accidental overflow
 	outputSize--;
-	char *information = "ASJ DLL - Version 0.0.1";
+	char *information = "ASJ authentication DLL - Version 0.0.1";
 	strncpy_s(output, outputSize, information, _TRUNCATE);
 }
 
@@ -36,25 +33,10 @@ void RVExtension(char *output, int outputSize, const char *function)
 	// Reduce output by 1 to avoid accidental overflow
 	outputSize--;
 
-	if (function == "info")
+	if (atoi(function) == 1)
 	{
 		char *text = "Arma Spotify Jukebox DLL - Version 0.0.1 - Please view https://github.com/Asaayu/Arma-Spotify-Jukebox for more information including important legal information.";
 		strncpy_s(output, outputSize, text, _TRUNCATE);
 		return;
 	};
-}
-
-// STRING callExtension ARRAY
-int RVExtensionArgs(char *output, int outputSize, const char *function, const char **argv, int argc)
-{
-	// Reduce output by 1 to avoid accidental overflow
-	outputSize--;
-
-	int index = 0;
-	int i;
-	for (i = 0; i < argc && index < outputSize; i++)
-	{
-		index += strncpy_s(output + index, outputSize - 1 - index, argv[i], _TRUNCATE);
-	}
-	return 0;
 }
